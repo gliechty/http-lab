70d4e1ab93e85d4aa4a915601e61007521c3cf9f
gliechty <guy.liechty@gmail.com>
2016-12-19 16:41:14 -0700

    show, add and delete work

---
+++ starter-code/frontend/js/criminalsController.js
@@ -0,0 +1,44 @@
+angular.module('TheCriminalsApp', [])
+	.controller('CriminalsController', CriminalsController);
+
+CriminalsController.$inject = ['$http'];
+
+function CriminalsController ($http){
+	var self = this;
+	this.all = [];
+	self.addCriminal = addCriminal;
+	self.newCriminal = {};
+	self.getCriminals = getCriminals;
+	self.deleteCriminal = deleteCriminal;
+
+
+	function getCriminals(){
+		$http
+			.get('http://localhost:3000/criminals')
+			.then(function(response){
+				self.all = response.data.criminals;
+			});
+	}
+	getCriminals();
+
+	function addCriminal(){
+		$http
+			.post('http://localhost:3000/criminals', self.newCriminal)
+			.then(function (request){
+				getCriminals();
+			});
+			self.newCriminal = {};
+	}
+
+	function deleteCriminal(criminal){
+		$http
+			.delete('http://localhost:3000/criminals/' + criminal._id)
+			.then(function (res){
+				var index = self.all.indexOf(criminal);
+				self.all.splice(index, 1);
+				getCriminals();
+			});
+	}
+
+}
+
